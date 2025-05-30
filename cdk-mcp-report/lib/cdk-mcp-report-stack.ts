eeab9fea168f30ce21284645c4e4d6169dfb76ac
John Park <kyopark2014@gmail.com>
2025-05-31 00:49:38 +0900

    update

--- cdk-mcp-report/lib/cdk-mcp-report-stack.ts
+++ cdk-mcp-report/lib/cdk-mcp-report-stack.ts
@@ -721,7 +721,7 @@ export class CdkMcpReportStack extends cdk.Stack {
       description: `environment-${projectName}`,
       exportName: `environment-${projectName}`
     });
-/*
+
     const commands = [
       'yum install git python-pip docker -y',
       'pip install pip --upgrade',
@@ -798,6 +798,6 @@ export class CdkMcpReportStack extends cdk.Stack {
     })
     listener.addAction(`RedirectHttpListener-for-${projectName}`, {
       action: defaultAction
-    });   */
+    });   
   }
 }
