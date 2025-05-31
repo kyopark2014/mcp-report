33ba688d4bd23ba85938c7f591b545e70f8f5698
John Park <kyopark2014@gmail.com>
2025-05-31 09:47:48 +0900

    update

--- cdk-mcp-report/lib/cdk-mcp-report-stack.ts
+++ cdk-mcp-report/lib/cdk-mcp-report-stack.ts
@@ -694,6 +694,7 @@ export class CdkMcpReportStack extends cdk.Stack {
       functionName: `lambda-rag-for-${projectName}`,
       code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../lambda-rag')),
       timeout: cdk.Duration.seconds(120),
+      memorySize: 4096,
       role: roleLambdaRag,
       environment: {
         bedrock_region: String(region),
